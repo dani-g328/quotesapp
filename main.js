063b966b0bd4319656b3e18f3cd87dcabd2a0f79
dani-g328 <70335247+dani-g328@users.noreply.github.com>
2022-01-21 12:57:48 -0500

    fixed issues

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function generate() {
   let ranNum = Math.floor(Math.random() * quoteArray.length);
-  document.querySelector("blockquote") = quoteArray[ranNum].text;
-  document.querySelector("cite") = quoteArray[ranNum].author;
+  document.querySelector("blockquote").innerText = quoteArray[ranNum].text;
+  document.querySelector("cite").innerText = quoteArray[ranNum].author;
+  console.log(ranNum);
 };
-console.log("Hello");
