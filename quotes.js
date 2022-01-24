50b1ff3f1056a5f7233ba0035dfc3044404efb03
dani-g328 <70335247+dani-g328@users.noreply.github.com>
2022-01-24 16:33:15 -0500

    got content aligned

--- quotes.js
+++ quotes.js
@@ -1,18 +1,39 @@
 const quoteArray = [
   {
-    "text": "This is me and you dude",
-    "author": "dani making soemethign up"
+    "text": "Do any of you have a completely unremarkable friend or maybe a houseplant I could go to dinner with on Saturday night?",
+    "author": "Miranda"
   },{
-    "text": "hello god cna you hear moi?",
-    "author": "dani again"
+    "text": "I curse the day you were born!",
+    "author": "Charlotte"
   },{
-    "text": "hey dude whats poppin",
-    "author": "cool people everywhere"
+    "text": "I like my money where I can see it- hanging in my closet.",
+    "author": "Carrie"
   },{
-    "text": "i didnt link my stylesheet bruh oh noar",
-    "author": "me when i havent yet done that"
+    "text": "I feel the same way about being a bridesmaid as you feel about Botox. Painful and unnecessary.",
+    "author": "Samantha"
   },{
-    "text": "check your old work!",
-    "author": "a smarter version of me"
+    "text": "The country runs better with a good looking man in the White House. I mean, look what happened with Nixon. No one wanted to f— him, so he f—ed everyone.!",
+    "author": "Samantha"
+  },{
+    "text": "There is a good way to break up with someone, and it doesn't... involve... a post it!",
+    "author": "Carrie"
+  },{
+    "text": "I’ve spent $40,000 on shoes and I have no place to live?! I will literally be the old woman who lived in her shoes.",
+    "author": "Carrie"
+  },{
+    "text": "I’ve been dating since I was fifteen! I’m exhausted! Where is he?",
+    "author": "Charlotte"
+  },{
+    "text": "I save my carbs for wine, it's called priorities.",
+    "author": "Carrie"
+  },{
+    "text": "I think I have monogamy. I caught it from you poeple.",
+    "author": "Samantha"
+  },{
+    "text": "The only thing I learned that night was that maybe I should have stayed home",
+    "author": "Carrie"
+  },{
+    "text": "Monogamy is on its way out again. It had a brief comeback in the 90s, but as the millennium approaches, everyone's leaving their options open.",
+    "author": "Stanford Blatch"
   }
 ]
